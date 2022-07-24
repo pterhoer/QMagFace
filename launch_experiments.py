24d5f71ed5e75040eced7bbddbe9ec51b64db4a5
Malte Ihlefeld <malte@famihlefeld.de>
2022-07-24 17:47:01 +0200

    add help text to experiments.py

--- launch_experiments.py
+++ launch_experiments.py
@@ -4,14 +4,14 @@ from experiments import single_image_benchmarks, runtime_analysis, robustness_an
 
 
 @click.command()
-@click.option('--train_db', default=None, type=click.Choice(['colorferet', 'adience', 'morph']))
-@click.option('--alpha', default=None, type=float)
-@click.option('--beta', default=None, type=float)
-@click.option('-r', '--dataset_root', default='_data/single_images/magface100', type=click.Path(exists=True))
-@click.option('-p', '--pairs_root', default='_data/pairs', type=click.Path(exists=True))
-@click.option('-t', '--test_db', type=click.Choice(['agedb', 'calfw', 'cfp', 'cplfw', 'lfw', 'xqlfw']), multiple=True)
-@click.option('--ijb_target', type=str, default='IJBC')
-@click.option('--experiment', type=click.Choice(['single_image', 'runtime', 'robustness', 'ijb', 'memory']))
+@click.option('--train_db', default=None, type=click.Choice(['colorferet', 'adience', 'morph']), help='The dataset which is used for finding alpha and beta.')
+@click.option('--alpha', default=None, type=float, help='The Alpha parameter of our method.')
+@click.option('--beta', default=None, type=float, help='The Beta Parameter of our method.')
+@click.option('-r', '--dataset_root', default='_data/single_images/magface100', type=click.Path(exists=True), help='The directory where the embeddings and filenames files are saved.')
+@click.option('-p', '--pairs_root', default='_data/pairs', type=click.Path(exists=True), help='The directory where all pairs.txt files are saved.')
+@click.option('-t', '--test_db', type=click.Choice(['agedb', 'calfw', 'cfp', 'cplfw', 'lfw', 'xqlfw']), multiple=True, help='The datasets to evaluate.')
+@click.option('--ijb_target', type=click.Choice(['IJBC', 'IJBB']), default='IJBC', help='Which IJB dataset to use.')
+@click.option('--experiment', type=click.Choice(['single_image', 'runtime', 'robustness', 'ijb', 'memory']), help='Which experiment to run.')
 def main(train_db, alpha, beta, dataset_root, pairs_root, test_db, ijb_target, experiment):
     if experiment == 'single_image':
         single_image_benchmarks.main(train_db, alpha, beta, dataset_root, pairs_root, test_db)
